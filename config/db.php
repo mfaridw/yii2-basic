50c3d565a713d5b87afa2b967661be096fbca981
mfaridw <mochfaridw@gmail.com>
2025-02-22 09:46:46 +0700

    Create country page

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;port=3309;dbname=yii2basic',
+    'username' => 'farid',
+    'password' => 'rahasia',
     'charset' => 'utf8',
 ];
